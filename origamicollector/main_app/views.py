c3e54137244d05073a777ab327442e5da3a90cc5
mkhan42 <michallekhan9@gmail.com>
2022-07-18 22:06:07 -0400

    created origami class'

--- origamicollector/main_app/views.py
+++ origamicollector/main_app/views.py
@@ -3,6 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+class Origami:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, color, description, age):
+    self.name = name
+    self.color = color
+    self.description = description
+    self.age = age
+
+origami = [
+  Origami('Paper Crane', 'Blue', 'Blue paper crane made with soft origami paper.', 2),
+  Origami('Dollar Shirt', 'Green', 'Folded collared shirt made with a $1 bill.', 1),
+  Origami('Dragon', 'Green', 'Green dragon origami made with metallic and shiny green origami paper.', 0)
+]
+
 
 def home(request):
   return HttpResponse('<h1>Hello Origami Collector</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def origami_index(request):
+  return render(request, 'origami/index.html', { 'origami': origami })
